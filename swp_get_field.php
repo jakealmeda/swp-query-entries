c2d4f9986d7486f166a8cec8c3a2131e885ad199
Jake Almeda <jakealmeda@gmail.com>
2019-05-14 13:09:58 +0800

    Update swp_get_field.php
    Added codes to handle querying of WP native fields

--- swp_get_field.php
+++ swp_get_field.php
@@ -61,8 +61,8 @@ class SWP_Get_Custom_Field {
 			    	
 			    	$this_id = $query[0]->id;
 			    	
-			    } else {
-			    	
+                } else {
+                    
 			    	$query = $wpdb->get_results( 'SELECT a.id FROM '.$wpdb->prefix.'posts AS a INNER JOIN '.$wpdb->prefix.'postmeta AS b ON a.id=b.post_id WHERE b.meta_value="'.$id.'"', OBJECT );
 			    	if( $query[0]->id ) {
 				    	$this_id = $query[0]->id;
@@ -70,27 +70,31 @@ class SWP_Get_Custom_Field {
 				    
 				    // Restore original Post Data
 					wp_reset_postdata();
-			    }
-			    
+                }
+                
 			}
 			
 		} else {
 			// retrieving from current post
 			$this_id = get_the_ID();
 		}
-
+        
 		// -----------------------------------------------
 		// GET CUSTOM FIELD DATA
 		$get_this = get_post_meta( $this_id, $field, TRUE );
+		if( !$get_this ) {
+			// GET WP NATIVE FIELD INSTEAD
+			$get_this = get_post_field( $field, $post_id, $context = 'display' );
+		}
 		// -----------------------------------------------
-
+        
 		// validate target
 		if( $target == "_blank" ) {
 			$targ = "target='".$target."'";
 		} else {
 			$targ = "";
 		}
-
+        
 		// check if field is image
 		if( wp_attachment_is_image( $get_this ) ) {
 			
@@ -98,37 +102,37 @@ class SWP_Get_Custom_Field {
 			
 			// check if link is for itself
 			if( $link == "_self" ) {
-
+                
 				$link = wp_get_attachment_image_src( $get_this, 'FULL' )[0];
 				$a_link = "<a href='".$link."' ".$targ.">".$this_image."</a>";
-
+                
 			} elseif( strtolower( $link ) == 'true' ) {
 				
 				$a_link = "<a href='".$get_this."' ".$targ.">".$this_image."</a>";
-
+                
 			} else {
-
+                
 				$a_link = $this_image;
-
+                
 			}
 			
 			return $a_link;
-
+            
 		} else {
 			
 			if( is_array( $get_this ) ) {
 				// ACF's flexible field
-
+                
 				/*foreach( $get_this as $key => $value ) {
 					echo $key." = ".$value."<br />";
 				}*/
 				//var_dump( $get_this );
 				
 			} else {
-
+                
 				// validate if link 				
 				if( strtolower( $link ) == 'true' ) {
-
+                    
 					// use content if indicated
 					if( $content ) {
 						$a_link = "<a href='".$get_this."' ".$targ.">".do_shortcode( $content )."</a>";
@@ -136,25 +140,25 @@ class SWP_Get_Custom_Field {
 						// use the link as the name
 						$a_link = "<a href='".$get_this."' ".$targ.">".do_shortcode( $get_this )."</a>";
 					}
-
+                    
 				} else {
 					$a_link = $get_this;
 				}
-
+                
 				return $a_link;
-
+                
 			}
 		}
-
+        
 	}
 
 	// CONSTRUCT
 	public function __construct() {
-
+        
 		if( !is_admin() ) {
 			add_shortcode( 'swp_get_field', array( $this, 'swp_gcf_main_func' ) );
 		}
-
+        
 	}
 
 }
